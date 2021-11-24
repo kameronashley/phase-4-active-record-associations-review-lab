594c59f1368f89462f11cf88664470defe89fc98
Kameron Ashley <kameronashley@gmail.com>
2021-11-24 07:00:35 -0800

    commit tree ef97265acad9e5b1474397a27427556f8b94095f

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,8 +1,10 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
       t.timestamps
+      t.belongs_to :taxi
+      t.belongs_to :passenger
+      
     end
   end
 end
